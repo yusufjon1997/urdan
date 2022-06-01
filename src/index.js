bacad985a36b496632bda6c13d5b95967b0a6752
Dev bro <tomakeitworth@gmail.com>
2022-06-01 13:44:47 +0500

    efewfwefwe

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -14,7 +13,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-    <BrowserRouter basename='/urdan'>
+    <BrowserRouter>
       <Provider store={store}>
           <PersistGate persistor={persistor}>
             <App />
