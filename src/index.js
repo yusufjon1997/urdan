0558c1f8b5dfaec38be28277050a7cf206ac254e
Dev bro <tomakeitworth@gmail.com>
2022-06-01 12:23:26 +0500

    initialized

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-    <BrowserRouter basename="/urdan-eccommerce">
+    <BrowserRouter>
       <Provider store={store}>
           <PersistGate persistor={persistor}>
             <App />
